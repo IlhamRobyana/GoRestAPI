d883582e8893259075361c6414fc95e847eb809f
IlhamRobyana <ilhamrobyanasofyan@gmail.com>
2019-07-10 14:47:23 +0700

    Updated the Struct

--- Main.go
+++ Main.go
@@ -2,51 +2,65 @@ package main
 
 import (
         "net/http"
-        //"strconv"
+        "strconv"
         "log"
+	"fmt"
+	"database/sql"
 
-
-        "github.com/jinzhu/gorm"
+        //"github.com/jinzhu/gorm"
         _ "github.com/lib/pq"
         "github.com/labstack/echo"
         "github.com/labstack/echo/middleware"
 )
 
 type User struct {
-        gorm.Model
-        id string `json:"id"`
-        age int `json:"age"`
-        first_name string `json:"first_name"`
-        last_name string `json:"last_name"`
-        email string `json:"email"`
+        Id		string	`json:"id"`
+        Age		string	`json:"age"`
+        First_name	string	`json:"first_name"`
+        Last_name	string	`json:"last_name"`
+        Email		string	`json:"email"`
+}
+
+type Users struct {
+	Users []User `json:"users"`
 }
 
 func createUser(c echo.Context) error {
 
-        db, err := gorm.Open("postgres","host=localhost port=5432 user=postgres dbname=test password=ujangbedil sslmode=disable")
+        db, err := sql.Open("postgres","host=localhost port=5432 user=postgres dbname=restapi_test password=ujangbedil sslmode=disable")
         if err != nil {
                 log.Panic(err)
         }
 
-        defer db.Close()
+        //defer db.Close()
 
         user := new(User)
         if err := c.Bind(user); err != nil {
                 return err
         }
+	fmt.Println(user.First_name)
+	age, _ := strconv.ParseInt(user.Age,10,64)
+	sqlStatement := "INSERT INTO users (first_name, last_name, age, email) VALUES ($1, $2, $3, $4)"
+	res, err := db.Query(sqlStatement, user.First_name, user.Last_name, age, user.Email)
+        if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+		return c.JSON(http.StatusCreated, user)
+	}
 
-        db.Create(&user)
-        return c.JSON(http.StatusCreated, user)
+	//db.Create(&user)
+        return c.String(http.StatusOK, "ok")
 }
 func main() {
 
-        db, err := gorm.Open("postgres","host=localhost port=5432 user=postgres dbname=test password=ujangbedil sslmode=disable")
-        if err != nil {
-                log.Panic(err)
-        }
+        //db, err := gorm.Open("postgres","host=localhost port=5432 user=postgres dbname=test password=ujangbedil sslmode=disable")
+        //if err != nil {
+          //      log.Panic(err)
+        //}
 
-        defer db.Close()
-        db.AutoMigrate(&User{})
+        //defer db.Close()
+        //db.AutoMigrate(&User{})
 
         e := echo.New()
 
